3565a7e541a2beedb553f71ec4a170b04c1158da
kosmos <riccardobarazzotto@gmail.com>
2025-09-10 18:47:00 +0200

    Add project files.

---
+++ VoidLevelEditor.cpp
@@ -0,0 +1,12 @@
+﻿// VoidLevelEditor.cpp : Defines the entry point for the application.
+//
+
+#include "VoidLevelEditor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
